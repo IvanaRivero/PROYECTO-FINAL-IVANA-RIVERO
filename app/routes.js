034406d16efa5ecb7f8cd6f6a57f76a5ab77eb05
IvanaRivero <86176393+IvanaRivero@users.noreply.github.com>
2021-09-09 20:11:51 -0300

    primer commit

---
+++ app/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+// Middlewares
+const auth = require('./middlewares/auth');
+
+// Policies
+const PostPolicy = require('./policies/PostPolicy');
+
+// Controllers
+const AuthController = require('./controllers/AuthController');
+const PostController = require('./controllers/PostController');
+
+// Home
+router.get('/', (req, res) => res.json({ hello: "World" }));
+
+// Dos rutas: login y registro
+// /api/singin & /api/singup
+router.post('/api/signin', AuthController.signIn);
+router.post('/api/signup', AuthController.signUp);
+
+// Rutas posts
+router.get('/api/posts', auth, PostController.index);
+router.get('/api/posts/:id', auth, PostController.find, PostPolicy.show, PostController.show);
+router.patch('/api/posts/:id', auth, PostController.find, PostPolicy.update,PostController.update);
+router.delete('/api/posts/:id', auth, PostController.find, PostPolicy.delete,PostController.delete);
+
+module.exports = router;
